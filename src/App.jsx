7cae07557b695cad4a6b487a101b4460d55105d3
TarasKing <ivanovtaras@live.ru>
2023-08-07 01:25:34 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+import Example from "./Example";
+function App() {
+  return (
+    <main>
+      {/* <Navbar /> */}
+      <Example />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,53 @@
+import { useState, useRef, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  const toggleLinks = () => {
+    // console.log(linksRef.current.getBoundingClientRect());
+    setShowLinks(!showLinks);
+  };
+
+  const linksStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : "0px",
+  };
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        {/* {showLinks && ( */}
+        <div
+          className="links-container"
+          ref={linksContainerRef}
+          style={linksStyles}
+        >
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        {/* )} */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
